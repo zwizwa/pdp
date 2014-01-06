287d887e33cbb9f1ab0a26a5ad8ae96eb92f687c
Tom Schouten <tom@zwizwa.be>
2014-01-06 13:40:48 -0500

    sync with last darcs

--- opengl/test/example01.gdb
+++ opengl/test/example01.gdb
@@ -1,8 +1,3 @@
-file /usr/local/bin/pd
-set new-console on
 set breakpoint pending on
 break glXSwapBuffers
 run
-
-
-
--- opengl/test/example01.sh
+++ opengl/test/example01.sh
@@ -1,3 +1,13 @@
 #!/bin/bash
 cd $(dirname $0)
-exec gdb -x example01.gdb -i=mi --args /usr/local/bin/pd -nogui -nrt -lib pdp example01.pd
+PD=/usr/local/bin/pd
+PDP=/home/tom/pdp
+exec gdb -x example01.gdb -i=mi --args $PD \
+-nogui -nrt -noprefs \
+-path $PDP \
+-path $PDP/abstractions \
+-path $PDP/opengl \
+-path $PDP/opengl/abstractions \
+-lib pdp \
+-lib pdp_opengl \
+example01.pd
